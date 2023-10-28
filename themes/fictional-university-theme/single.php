30aa17501471cfc319c4725072cb298c4c94c82a
Rajneesh Kumar <rishulpu1@gmail.com>
2023-10-28 10:11:10 +0530

    created blog and front-page templates

--- themes/fictional-university-theme/single.php
+++ themes/fictional-university-theme/single.php
@@ -3,8 +3,26 @@ get_header();
 while(have_posts()){
     the_post();
 ?>
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
+
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>Keep up with our latest news.</p>
+        </div>
+      </div>
+    </div>
+    <div class="container container--narrow page-section">
+      
+          <div class="post-item">
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_content(); ?></p>
+          </div>
+       
+    </div>
+
+
 
 <?}
 
