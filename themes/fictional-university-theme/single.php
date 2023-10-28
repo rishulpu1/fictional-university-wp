6202064395c529a711e33dc2dd37814cb1574391
Rajneesh Kumar <rishulpu1@gmail.com>
2023-10-28 22:52:11 +0530

    added pagination and created single.php

--- themes/fictional-university-theme/single.php
+++ themes/fictional-university-theme/single.php
@@ -14,11 +14,14 @@ while(have_posts()){
       </div>
     </div>
     <div class="container container--narrow page-section">
-      
-          <div class="post-item">
-            <h2><?php the_title(); ?></h2>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+                <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted By <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(", ") ?></span>
+            </p>
+        </div>
+        <div class="generic-content">
             <p><?php the_content(); ?></p>
-          </div>
+        </div>
        
     </div>
 
