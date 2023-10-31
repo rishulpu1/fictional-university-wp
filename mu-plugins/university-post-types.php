3fa1241169d1563b773e41fbf168bb275562101c
Rajneesh Kumar <rishulpu1@gmail.com>
2023-10-31 23:02:17 +0530

    added support of gutenburg for custom post types

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -3,6 +3,7 @@
 function university_post_types(){
     register_post_type('event', array(
         'public' => true,
+        'show_in_rest' => true,
         'labels' => array(
             'name' => 'Events',
             'add_new_item' => 'Add New Event',
