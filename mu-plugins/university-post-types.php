0e6c480dd67199b86854e58709aebeac499633c5
Rajneesh Kumar <rishulpu1@gmail.com>
2023-11-07 01:27:33 +0530

    created prorams cutom field

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php 
 
 function university_post_types(){
+    // Event Post Type
     register_post_type('event', array(
         'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array(
@@ -17,6 +18,25 @@ function university_post_types(){
             'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
+    ));
+
+    // Program Post Type
+    register_post_type('program', array(
+        'supports' => array('title', 'editor'),
+        'rewrite' => array(
+            'slug' => 'prorams'
+        ),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
     ));
 }
 add_action('init', 'university_post_types');
