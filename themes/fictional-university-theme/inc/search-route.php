f25713f82328815a69599a3ea4b87a4a69290cf3
Rajneesh Kumar <rishulpu1@gmail.com>
2023-12-07 23:48:23 +0530

    updated search logic

--- themes/fictional-university-theme/inc/search-route.php
+++ themes/fictional-university-theme/inc/search-route.php
@@ -44,7 +44,8 @@ function universitySearchResults($data){
         if(get_post_type() == 'program'){
             array_push($results['programs'], array(
                 'title' => get_the_title(),
-                'permalink' => get_the_permalink()
+                'permalink' => get_the_permalink(),
+                'id' => get_the_id()
             ));
         }
 
@@ -73,7 +74,34 @@ function universitySearchResults($data){
         }
         
     }
+    if($results['programs']){
+        $programsMetaQuery = array('relation' => 'OR');
 
+    foreach($results['programs'] as $item){
+        array_push($programsMetaQuery,array(
+            'key' => 'related_program',
+            'compare' => 'LIKE',
+            'value' => '"'. $item['id'] .'"'
+        ) );
+    }
 
+    $programRelationship = new WP_Query(array(
+        'post_type' => 'professor',
+        'meta_query' => $programsMetaQuery
+    ));
+
+    while($programRelationship->have_posts()){
+        $programRelationship->the_post();
+        if(get_post_type() == 'professor'){
+            array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+            ));
+        }
+    }
+    $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+    }
+    
     return $results;
 }
